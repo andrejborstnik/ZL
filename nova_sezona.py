9cf44e2fd2893acba702926313953d58e784365d
unknown <andrej-borstnik@hotmail.com>
2013-11-17 12:00:47 +0100

    Tekoča sezona.

--- nova_sezona.py
+++ nova_sezona.py
@@ -1,15 +1,15 @@
-with open('ZL_2011_2012.csv','r',encoding='utf-8')as f:
+with open('ZL_2012_2013.csv','r',encoding='utf-8')as f:
     with open('zacetek.txt','w',encoding='utf-8')as g:
         b=0
-        for i in f:
+        for i in f.readlines():
             b+=1
             a=0
             if b!=1:
                 for j in i.split(';'):
                     a+=1
-                    if a in [1,2,3,4,25]:
+                    if a in [1,2,3,4,21]:
                         g.write(j)
-                        if a==25:
+                        if a==21:
                             pass#g.write('\n')
                         else:
                             g.write(';')
