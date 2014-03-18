d1ab128f8959c62a4525f56af60ca26b4961ead3
unknown <andrej-borstnik@hotmail.com>
2014-03-18 18:33:09 +0100

    Priprava za naslednjo sezono.

--- nova_sezona.py
+++ nova_sezona.py
@@ -1,4 +1,4 @@
-with open('ZL_2012_2013.csv','r',encoding='utf-8')as f:
+with open('ZL_2013_2014.csv','r',encoding='utf-8')as f:
     with open('zacetek.txt','w',encoding='utf-8')as g:
         b=0
         for i in f.readlines():
@@ -7,10 +7,10 @@ with open('ZL_2012_2013.csv','r',encoding='utf-8')as f:
             if b!=1:
                 for j in i.split(';'):
                     a+=1
-                    if a in [1,2,3,4,23]:
+                    if a in [1,2,3,4,25]:
                         g.write(j)
-                        if a==23:
-                            #g.write('\n')
+                        if a==25:
+                            g.write('\n')
                             pass
                         else:
                             g.write(';')
