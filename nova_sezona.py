baeb341c705c470e8dda00f8ef4507c76af74f50
andrejborstnik <andrej-borstnik@hotmail.com>
2014-11-16 19:27:36 +0100

    Popravek Igorja.

--- nova_sezona.py
+++ nova_sezona.py
@@ -1,5 +1,5 @@
 with open('ZL_2013_2014.csv','r',encoding='utf-8')as f:
-    with open('zacetek.txt','w',encoding='utf-8')as g:
+    with open('zacetek1.txt','w',encoding='utf-8')as g:
         b=0
         for i in f.readlines():
             b+=1
