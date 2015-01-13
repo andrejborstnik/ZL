6d21f3264a52fac9994bc6d0349ed40c19bf40e1
andrejborstnik <aborstnik@klemen-pc.ijs.si>
2015-01-13 10:30:42 +0100

    Obdelava rezultatov zl14.

--- Rezultati/stupiditalians-normal.py
+++ Rezultati/stupiditalians-normal.py
@@ -2,37 +2,49 @@ def pretvori(niz):
     niz1=niz.split(' ')
     b=''
     for niz in niz1:
-        b+=niz[0]+niz[1:].lower()+' '
+        if niz:
+            b+=niz[0]+niz[1:].lower()+' '
     return b[:-1]
-with open('ZL10.txt','r',encoding='utf-8') as f:
-    with open('ZL10.csv','w',encoding='utf-8')as g:
-        kat='A'
+with open('ZL14a.txt','r',encoding='utf-8') as f:
+    with open('ZL14a.csv','w',encoding='utf-8')as g:
+        kat='C'
         g.write('@SI\n')
-        g.write('Pl;First name;Surname;Time;Short;City;Classifier;\n')
+        g.write('Pl.;First name;Surname;Time;Short;City;Classifier;\n')
         for i in f:
             a=i.split(';')
             if i!='\n':
                 ime=''
-                for k in a[3].split(' ')[1:]:
+                if (len(a[1].split(' '))>2):
+                    print(a[1])
+                for k in a[1].split(' ')[1:]:
                     ime+=k+' '
                 ime=pretvori(ime[:-1])
-                priimek=pretvori(a[3].split(' ')[0])
-                if a[2][0]=='=':
+                priimek=pretvori(a[1].split(' ')[0])
+                if a[3][0] in ['=','M']:
                     cas='mp'
                     clas='3'
+                elif a[3]=='DNF':
+                    cas = 'mp'
+                    clas = '2'
                 else:
                     cas=a[2]
                     clas='0'
-                klub=a[8]
+                klub=a[3]
                 mesto=a[0]
-                if mesto:
+                if mesto == '-':
+                    mesto = ''
+                elif mesto:
                     mesto+='.'
                 if mesto and mesto[0]=='\ufeff':
                     mesto=mesto[1:]
                 g.write(mesto+';'+ime+';'+priimek+';'+cas+';'+kat+';'+klub+';'+clas+';\n')
-            elif kat=='A':
+            elif kat=='C':
                 kat='B'
             elif kat=='B':
-                kat='C'
+                kat='A'
+            elif kat=='A':
+                kat='Skupine'
+            elif kat=='Skupine':
+                kat='Otroci'
                 
             
