77414036fd438dd09985a179b99c62a49ac4b221
andrejborstnik <andrej-borstnik@hotmail.com>
2016-11-01 18:56:55 +0100

    Začetek.

--- nova_sezona.py
+++ nova_sezona.py
@@ -1,4 +1,4 @@
-with open('ZL_2014_2015.csv','r',encoding='utf-8')as f:
+with open('ZL_2015_2016.csv','r',encoding='utf-8')as f:
     with open('zacetek1.txt','w',encoding='utf-8')as g:
         b=0
         for i in f.readlines():
@@ -7,11 +7,11 @@ with open('ZL_2014_2015.csv','r',encoding='utf-8')as f:
             if b!=1:
                 for j in i.split(';'):
                     a+=1
-                    if a in [1,2,3,4,30]:
-                        g.write(j)
-                        if a==30:
+                    if a in [1,2,3,4,29]:
+                        if a==29:
                             #g.write(str(int(int(j)*1.15)))
-                            #g.write('\n')
-                            pass
+                            g.write(j[:-1])
+                            g.write('\n')
                         else:
+                            g.write(j)
                             g.write(';')
