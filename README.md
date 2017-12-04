75202cfdc1da7425e5348f9df8eb4fd90dc67039
Klemen Kenda <klemen.kenda@ijs.si>
2017-12-04 01:10:43 +0100

    Začetno stanje sezone 2017/2018. Dodani rezultati do ZL6.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-ZL
-==
+#Izračun zimske lige
+
+## Nalaganje rezultatov
+Rezultate v CSV obliki naložimo v direktorij ```Rezultati```. CSV mora biti v formatu OE2003. Lahko tudi OEvent-ova verzija OE2003.
+
+## Začetno stanje
+Začetno stanje je zabeleženo v ```Zacetek.txt``` in določa osnovno vrednost tekmovalcev pred začetkom lige. Temelji na izračunu zadnje lige. Novo začetno datoteko pripravimo s pomočjo ```nova_sezona.py```. Rezultat, ki je v datoteki ```zacetek1.txt``` preimenujte v ```Zacetek.txt```.
--- nova_sezona.py
+++ nova_sezona.py
@@ -1,4 +1,4 @@
-with open('ZL_2015_2016.csv','r',encoding='utf-8')as f:
+with open('ZL_2016_2017.csv','r',encoding='utf-8')as f:
     with open('zacetek1.txt','w',encoding='utf-8')as g:
         b=0
         for i in f.readlines():
@@ -7,10 +7,13 @@ with open('ZL_2015_2016.csv','r',encoding='utf-8')as f:
             if b!=1:
                 for j in i.split(';'):
                     a+=1
-                    if a in [1,2,3,4,29]:
-                        if a==29:
+                    if a in [1,2,3,4,30]:
+                        if a==30:
                             #g.write(str(int(int(j)*1.15)))
-                            g.write(j[:-1])
+                            # naredimo še razteg okoli 1000
+                            tocke = int(j[:-1])
+                            tocke = int(tocke + (tocke - 1000) * 0.2)
+                            g.write(str(tocke))
                             g.write('\n')
                         else:
                             g.write(j)
